34d48486e4bacf843c2526bf049ee90fc33d7656
Pri_030 <priyankasingh30@gmail.com>
2020-08-11 16:05:11 -0400

    added portfolio components

---
+++ src/components/Portfolio.js
@@ -0,0 +1,107 @@
+import React from "react";
+import "./style.css";
+
+
+export default function Portfolio() {
+  return (
+    <section id="portfolio" className="portfolio">
+      <div className="container">
+        <div className="section-title">
+          <h2>Portfolio</h2>
+          <p>Please click on the "More Details" link for project details</p>
+        </div>
+
+        <div
+          className="row portfolio-container"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div className="portfolio-wrap">
+              <img
+                src="assets/img/portfolio/booktivist-App.jpg"
+                className="img-fluid"
+                alt=""
+              ></img>
+              <div className="portfolio-links">
+                <a href="booktivist-details.html" title="More Details">
+                  <i className="bx bx-link"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div className="portfolio-wrap">
+              <img
+                src="assets/img/portfolio/booktivist-App.jpg"
+                className="img-fluid"
+                alt=""
+              ></img>
+              <div className="portfolio-links">
+                <a href="booktivist-details.html" title="More Details">
+                  <i className="bx bx-link"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div className="portfolio-wrap">
+              <img
+                src="assets/img/portfolio/booktivist-App.jpg"
+                className="img-fluid"
+                alt=""
+              ></img>
+              <div className="portfolio-links">
+                <a href="booktivist-details.html" title="More Details">
+                  <i className="bx bx-link"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div className="portfolio-wrap">
+              <img
+                src="assets/img/portfolio/booktivist-App.jpg"
+                className="img-fluid"
+                alt=""
+              ></img>
+              <div className="portfolio-links">
+                <a href="booktivist-details.html" title="More Details">
+                  <i className="bx bx-link"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div className="portfolio-wrap">
+              <img
+                src="assets/img/portfolio/booktivist-App.jpg"
+                className="img-fluid"
+                alt=""
+              ></img>
+              <div className="portfolio-links">
+                <a href="booktivist-details.html" title="More Details">
+                  <i className="bx bx-link"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div className="portfolio-wrap">
+              <img
+                src="assets/img/portfolio/booktivist-App.jpg"
+                className="img-fluid"
+                alt=""
+              ></img>
+              <div className="portfolio-links">
+                <a href="booktivist-details.html" title="More Details">
+                  <i className="bx bx-link"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
