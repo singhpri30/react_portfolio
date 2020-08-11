3ef045b0e2f2b9f9cfc70c1f78f6643bee75fb1c
Pri_030 <priyankasingh30@gmail.com>
2020-08-11 13:47:40 -0400

    added necesssary packages

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer id="footer">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Priyanka Singh</span></strong>
+            </div>
+        </div>
+    </footer>
+    )
+}
