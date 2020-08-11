5ce98a35c03053263a9a43712ea01ed702f8fc24
Pri_030 <priyankasingh30@gmail.com>
2020-08-11 00:33:30 -0400

    Updates

---
+++ precache-manifest.488d8af47ee05685f335ae35f5a08dbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6daf7a83e5043799446314da9d6e835",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "f9e65be7edd80d5bbae8",
+    "url": "/react_portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "26b17970224284f7292b",
+    "url": "/react_portfolio/static/js/2.8570fe15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_portfolio/static/js/2.8570fe15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e65be7edd80d5bbae8",
+    "url": "/react_portfolio/static/js/main.61f39752.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
